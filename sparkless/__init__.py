3dd50b8e6a8bd31d64c48c142fa1c40d688fc726
Odo Matthews <odosmatthews@gmail.com>
2025-12-21 15:17:55 -0500

    Bump version to 3.18.4

--- sparkless/__init__.py
+++ sparkless/__init__.py
@@ -102,7 +102,7 @@ from .errors import (  # noqa: E402
 #   - sparkless.data_generation - Test data generation
 # ==============================================================================
 
-__version__ = "3.18.3"
+__version__ = "3.18.4"
 __author__ = "Odos Matthews"
 __email__ = "odosmatthews@gmail.com"
 
